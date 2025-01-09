a2e44abd3984767e07cbcc671ecf8593fcab820e
permeable11 <sonawanekrishna276@gmail.com>
2025-01-09 11:54:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
+API_ID    = os.environ.get("API_ID", "21763365")
+API_HASH  = os.environ.get("API_HASH", "5ff490c712f9a9f139a115b16f83f497")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 WEBHOOK = True  # Don't change this
